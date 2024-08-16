41b5cd919eea4fab2e6432bf60a6dd8d4c05a423
leaviki90 <leaviki90@gmail.com>
2024-08-17 00:41:26 +0200

    Static data moved to Fixtures, comments added

--- cypress/e2e/UserAuthentication.cy.js
+++ cypress/e2e/UserAuthentication.cy.js
@@ -1,75 +1,93 @@
-const email = `tester_${Date.now()}@test.com`; //with the timestamp
-const password = "12345678";
-const firstName = "Lea";
-const lastName = "Lastname";
-const company = "Somecompany";
-const address = "Kržince bb";
-const address2 = "Kržince bb"; 
-const country = "Serbia"; 
-const state = "Serbia";
-const city = "Niš bato"; 
-const zipcode = "18000";
-const mobileNumber = "0631234567";
+
 const expectedURL = "https://www.automationexercise.com/";
+let user;
+let userEmail;
+
+before(() => {
+  //Generating a dynamic email only once before all tests
+  userEmail = `tester_${Date.now()}@test.com`;
+});
 
 
 
-describe('User Authentication Suite', () => {
-  it('Register a new user', () => {
-    cy.visit('https://www.automationexercise.com/');
+beforeEach(() => {
+    // Load user details from the fixture file before each test
+    cy.fixture('userDetails').then((userData) => {
+        user = userData;// Assign the data loaded from the fixture to the 'user' variable
+        user.email = userEmail; // Update the 'email' property of the 'user' object with the dynamically generated email
+        
+    });
+});
+
+
+describe('User Authentication Suite', function()  {
+  it('Register a new user', function() {
+    cy.visit(expectedURL);
     cy.url().should("include", "automationexercise");
     cy.title().should('eq', 'Automation Exercise');
+
+    // Go to the Signup page
     cy.get("a[href='/login']").click();
     cy.get(".signup-form h2").contains("New User Signup!"); //Assertion
-    cy.xpath("//input[@placeholder='Name']").type(firstName);
-    cy.xpath("//input[@data-qa='signup-email']").type(email);
+    cy.xpath("//input[@placeholder='Name']").type(user.firstName);
+    cy.xpath("//input[@data-qa='signup-email']").type(user.email);
     cy.xpath("//button[text()='Signup']").click();
+
+    // Enter account information
     cy.xpath("//b[contains(text(), 'Enter Account Information')]").should("have.text", "Enter Account Information");
     cy.get("#id_gender2").check();
-    cy.get("#password").type(password);
+    cy.get("#password").type(user.password);
     cy.get("#days").select("30");
     cy.get("#months").select("December");
     cy.get("#years").select("1990");
     cy.get("#newsletter").check();
     cy.get("#optin").check();
-    cy.get("#first_name").type(firstName);
-    cy.get("#last_name").type(lastName);
-    cy.get("#company").type(company);
-    cy.get("#address1").type(address);
-    cy.get("#address2").type(address2);
+    cy.get("#first_name").type(user.firstName);
+    cy.get("#last_name").type(user.lastName);
+    cy.get("#company").type(user.company);
+    cy.get("#address1").type(user.address);
+    cy.get("#address2").type(user.address2);
     cy.get("#country").select("Australia");
-    cy.get("#state").type(state);
-    cy.get("#city").type(city);
-    cy.get("#zipcode").type(zipcode);
-    cy.get("#mobile_number").type(mobileNumber);
+    cy.get("#state").type(user.state);
+    cy.get("#city").type(user.city);
+    cy.get("#zipcode").type(user.zipcode);
+    cy.get("#mobile_number").type(user.mobileNumber);
     cy.get("button[data-qa='create-account']").click();
+
+    // Validate account creation
     cy.get("h2.title > b").should("contain", "Account Created!");
     cy.get("a[data-qa='continue-button']").click();
-    cy.xpath(`//a[contains(text(), ' Logged in as ')]/b[contains(text(), '${firstName}')]`).should('be.visible');
+    cy.xpath(`//a[contains(text(), ' Logged in as ')]/b[contains(text(), '${user.firstName}')]`).should('be.visible');
+    
+    // Logout
     cy.get("a[href='/logout']").click();
-    
-    
-    
   })
 
-  it('Login with correct email and password', () => {
-    cy.visit('https://www.automationexercise.com/');
-    //implicit asertion
-    cy.url().should("eq", expectedURL);
+  it('Login with correct email and password', function() {
+    cy.visit(expectedURL);
+
+    // Validate URL and page elements
+    cy.url().should("eq", expectedURL); //implicit asertion
     cy.xpath("//img[@alt='Website for automation practice']").should("exist").and("be.visible");
+    
+    // Go to the Login page and login with valid credentials
     cy.get("a[href='/login']").click();
     cy.get(".login-form > h2").should("contain", "Login to your account").and("be.visible");
-    cy.get("input[data-qa='login-email']").type(email);
-    cy.get("input[placeholder='Password']").type(password);
+    cy.get("input[data-qa='login-email']").type(user.email);
+    cy.get("input[placeholder='Password']").type(user.password);
     cy.get("button[data-qa='login-button']").click();
-    cy.xpath(`//a[contains(text(), ' Logged in as ')]/b[contains(text(), '${firstName}')]`).should('be.visible');
+
+    // Validate successful login
+    cy.xpath(`//a[contains(text(), ' Logged in as ')]/b[contains(text(), '${user.firstName}')]`).should('be.visible');
+    
+    // Delete account
     cy.get("a[href='/delete_account']").click();
     cy.get("h2.title > b").should("be.visible").and("contain", "Account Deleted!");
   })
 
- // These two test cases are logically connected. The order of execution should not be changed.
+  // These two test cases are logically connected. The order of execution should not be changed.
 
-  
+
 })
 
 
