b333982e14ed77edff780fd49b6330792c3af371
leaviki90 <leaviki90@gmail.com>
2024-08-20 16:31:12 +0200

    TC: Search products

--- cypress/e2e/Product&CartManagement.cy.js
+++ cypress/e2e/Product&CartManagement.cy.js
@@ -1,3 +1,4 @@
+const productName = "winter";
 
 describe("Product and Cart Management", () => {
     it("Verify All Products and Product Details page", () => {
@@ -34,6 +35,49 @@ describe("Product and Cart Management", () => {
         cy.get('.product-information p').eq(1).should('be.visible').and('contain.text', 'Availability: In Stock');
         cy.get('.product-information p').eq(2).should('be.visible').and('contain.text', 'Condition: New');
         cy.get('.product-information p').eq(3).should('be.visible').and('contain.text', 'Brand: Polo');
+    })
+
+
+    it("Search products", () => {
+        cy.visit("/");
+
+        // Validate URL and page elements
+        cy.url().should("include", "automationexercise");
+        cy.title().should('eq', 'Automation Exercise');
+        cy.xpath("//img[@alt='Website for automation practice']").should("exist").and("be.visible");
+
+        //Click on products and verify that All products page is opened 
+        cy.get("a[href='/products']").click();
+        cy.get("a[href='/products']").should("have.css", "color", "rgb(255, 165, 0)");
+        cy.url().should("contain", "products");
+        cy.title().should("contain", "Automation Exercise - All Products");
+
+        //Enter product name in search input and click search button
+        cy.get("#search_product").type(productName);
+        cy.get("#search_product").should("have.value", productName);
+        cy.get("#submit_search").click();
+
+        //Verify 'SEARCHED PRODUCTS' is visible
+        cy.get(".title.text-center").should("exist").and("be.visible").and("contain", "Searched Products");
+
+        //Verify all the products related to search are visible
+        cy.get("body").then($body => {
+            if ($body.find(".productinfo").length > 0) {
+                // If product exist, the each loop starts
+                cy.get(".productinfo").should("be.visible").each(($product) => {
+                    cy.wrap($product).within(() => {
+                        cy.get("p").invoke("text").then((text) => {
+                            expect(text.toLowerCase()).to.include(productName);
+                        });
+                    });
+                });
+                cy.get(".productinfo").should('have.length.greaterThan', 0);
+            } else {
+                // If product doesn't exist, send message
+                cy.log("No such product");
+            }
+        });
+
     })
 
 
